9b3afc51440015fbba7f8bba1d6f8364d7a0e3e9
lululthfh <punyalulu05@gmail.com>
2026-01-02 20:20:15 +0700

    feat: menambahkan peta navigasi

---
+++ app/src/main/java/com/example/datasiswa/ui/PetaNavigasi.kt
@@ -0,0 +1,39 @@
+package com.example.datasiswa.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.datasiswa.ui.route.DestinasiDetail
+import com.example.datasiswa.ui.route.DestinasiEntry
+import com.example.datasiswa.ui.route.DestinasiHome
+
+@Composable
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier= Modifier
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route,
+        modifier= Modifier){
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                navigateToItemUpdate = {navController.navigate("${DestinasiDetail.route}/${it}")}
+            )
+        }
+        composable(DestinasiEntry.route)
+        {
+            EntrySiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route)})
+        }
+    }
+}
