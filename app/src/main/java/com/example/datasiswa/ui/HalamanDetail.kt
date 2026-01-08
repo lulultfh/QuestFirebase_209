92afefaea2a1a5f6ce7551eb2a8b3a4c71252d7e
lululthfh <punyalulu05@gmail.com>
2026-01-08 17:01:17 +0700

    feat: menambahkan private function BarisDataDetail pada halaman detail

--- app/src/main/java/com/example/datasiswa/ui/HalamanDetail.kt
+++ app/src/main/java/com/example/datasiswa/ui/HalamanDetail.kt
@@ -46,6 +46,17 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterial3Api::class)
 
 @Composable
+private fun BarisDataDetail(
+    @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
+){
+    Row(modifier = modifier) {
+        Text(stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
+}
+
+@Composable
 private fun DeleteConfirmationDialog(
     onDeleteConfirm: () -> Unit,
     onDeleteCancel: () -> Unit,
